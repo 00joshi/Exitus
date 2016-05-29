19d22033369a23713d8413f0a31305ca0bb1bd44
Jochen Luedering <j@chaotisch.org>
2016-05-30 00:45:07 +0200

    refactoring and reset after game

--- exitus.py
+++ exitus.py
@@ -20,16 +20,12 @@ from kivy.uix.modalview import ModalView
 from time import time
 from kivy.core.audio import SoundLoader
 ## Global Vars
-guylist = list()
-holelist = list()
 wsize = (800,600)
-nguys = 15
+nguys = 10
 nholes = 5
-nkills = 0
 sndscream = SoundLoader.load('scream.wav')
 sndhammer = SoundLoader.load('hammer.wav')
 sndfanfare = SoundLoader.load('fanfare.wav')
-starttime = time()
 Config.set('graphics', 'width', wsize[0])
 Config.set('graphics', 'height', wsize[1])
 class ExitusGuy(Widget):
@@ -45,7 +41,17 @@ class ExitusGuy(Widget):
         self.velocity = Vector(2, 0).rotate(randint(0,360))
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
-        for hole in holelist:
+       # bounce off top and bottom
+        if (self.y < 0): 
+            self.velocity_y *= -1
+        if (self.top > game.top):
+            self.velocity_y *= -1
+       # bounce off left and right
+        if (self.x < 0):
+            self.velocity_x *= -1
+        if (self.right > game.width):
+            self.velocity_x *= -1
+        for hole in ExitusGame.holelist:
             if hole.disabled<=0:
                 self.collision(hole)
     def collision(self, hole):
@@ -82,59 +88,59 @@ class ExitusHole(Widget):
 
 class ExitusGame(Widget):
     fanfare = sndfanfare
+    starttime = NumericProperty(0)
+    guylist = list()
+    holelist = list()
+    nkills = NumericProperty(0)
+    def GameOverNote(self,survivaltime):
+        popup = ModalView(size_hint=(0.75, 0.5))
+        victory_label = Label(text="Game Over\n" + "%0.2f"%survivaltime + " sec", font_size=50)
+        popup.add_widget(victory_label)
+        popup.bind(on_dismiss=game.reset)
+        popup.bind(on_press=popup.dismiss)
+        popup.open()
     def __init__(self,**kwargs):
         super(ExitusGame, self).__init__(**kwargs)
+        self.starttime = time()
+        self.nkills = 0
+        Clock.schedule_interval(self.update,1.0/60.0)
+        Clock.schedule_interval(self.difficultify,5)
         for i in range(0,nguys):
-            guylist.append(ExitusGuy())
+            self.guylist.append(ExitusGuy())
         for i in range(0,nholes):
-            holelist.append(ExitusHole())
-        for thing in guylist+holelist:
+            self.holelist.append(ExitusHole())
+        for thing in self.guylist+self.holelist:
             self.add_widget(thing)
+    def reset(self,origin):
+        print("resetting")
+        self.guylist.clear()
+        self.holelist.clear()
+        self.__init__()
     def scoreupdate(self):
-        global nkills
-        nkills+=1
-        if nkills == nguys-3:
+        self.nkills+=1
+        if self.nkills == nguys-3:
             self.gameover()
     def gameover(self):
-            survival = time() -starttime
-            popup = ModalView(size_hint=(0.75, 0.5))
-            victory_label = Label(text="Game Over\n" + "%0.2f"%survival + " sec", font_size=50)
-            popup.add_widget(victory_label)
-            for guy in guylist:
-                guy.velocity = (0,0)
-            #popup.bind(on_dismiss=self.reset)
-            popup.open()
+            survival = time() - self.starttime
+            self.clear_widgets()
+            Clock.unschedule(self.update)
+            Clock.unschedule(self.difficultify)
+            self.GameOverNote(survival)
             self.fanfare.play()
     def difficultify(self,dt):
         newhole = ExitusHole()
         self.add_widget(newhole)
-        holelist.append(newhole)
+        self.holelist.append(newhole)
     def update(self,dt):
-        for guy in guylist:
+        for guy in self.guylist:
             guy.move()
-       # bounce off top and bottom
-            if (guy.y < 0): 
-                guy.velocity_y *= -1
-#                guy.pos[1] = 0
-            if (guy.top > self.top):
-                guy.velocity_y *= -1
-#                guy.pos[1] = self.height -30
-       # bounce off left and right
-            if (guy.x < 0):
-                guy.velocity_x *= -1
-#                guy.pos[0]=0
-            if (guy.right > self.width):
-                guy.velocity_x *= -1
-#                guy.pos[0]=self.width -30
-        for hole in holelist:
+        for hole in self.holelist:
             hole.update()
+
 class ExitusApp(App):
     def build(self):
         game=ExitusGame()
-        Clock.schedule_interval(game.update,1.0/60.0)
-        Clock.schedule_interval(game.difficultify,5)
         global game
         return game
-
 if __name__ == '__main__':
     ExitusApp().run()
